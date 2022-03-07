31d605f16ab050a3dbaa445ea8c9891792cc1f8b
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2022-03-07 14:16:19 +0800

    Adding views
    Adding admin views and trainer views

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-const http = require('http');
-const fs = require('fs')
+const http = require('http')
+const express = require('express')
+const app = express()
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200,{ 'Content-Type':'text/html' })
+//Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/images', express.static(__dirname + 'public/images'))
+app.use('/javascripts', express.static(__dirname + 'public/javascripts'))
 
+//Set Views
+app.set('views', '/views')
+app.set('view engine','ejs')
 
-  fs.readFile('index.html', function(error, data) {
-      if (error){
-          res.writeHead(404)
-          res.write('Error: File Not found')
-      } else{
-          res.write(data)
-      }
-      
-      res.end();
-  })
-  
-  
-});
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
 
+})
+app.get('/trainer', (req, res) => {
+    res.sendFile(__dirname + '/views/trainer.html')
 
-server.listen(port, hostname, (error) => {
-    if (error) {
-        console.log('Something went wrong', error)
-    }else {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    }
-  
-});
+})
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/views/admin.html')
+
+})
+
+//Listening on port
+app.listen( port, hostname, (error) => console.log(`Server running at http://${hostname}:${port}/`))
 
